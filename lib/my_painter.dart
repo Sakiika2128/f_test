d003bfb3892c5c68412afe39c5b7febeecd19576
Sakiika2128 <sakiikat7064@bside-label.com>
2023-06-29 18:41:19 +0900

    楕円の描写

--- lib/my_painter.dart
+++ lib/my_painter.dart
@@ -7,15 +7,20 @@ class MyPainter extends CustomPainter{
     Paint p = Paint();
 
     p.style = PaintingStyle.fill;
-    p.color = Color.fromARGB(150, 0, 200, 255);
-    Rect r = Rect.fromLTWH(50, 50, 150, 150);
-    canvas.drawRect(r, p);
+    p.color = Color.fromARGB(150, 127, 255, 212);
+    Offset ctr = Offset(100, 100);
+    canvas.drawCircle(ctr, 80, p);
+
+    p.style = PaintingStyle.fill;
+    p.color = Color.fromARGB(255, 255, 255, 0);
+    Rect r = Rect.fromLTWH(100, 100, 200, 200);
+    canvas.drawOval(r, p);
 
     p.style = PaintingStyle.stroke;
-    p.color = Color.fromARGB(150, 200, 0, 255);
-    r = Rect.fromLTWH(100, 100, 150, 150);
-    p.strokeWidth = 10.0;
-    canvas.drawRect(r, p);
+    p.color = Color.fromARGB(255, 255, 192, 203);
+    p.strokeWidth = 10;
+    r = Rect.fromLTWH(100, 100, 100, 100);
+    canvas.drawOval(r, p);
 
   }
   @override 
