f9f1eed829aba0a3784940833a29bfe7ed1ff2cd
Sakiika2128 <sakiikat7064@bside-label.com>
2023-07-03 16:57:02 +0900

    座標軸を変更して図形を描画

--- lib/my_painter.dart
+++ lib/my_painter.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:f_test/first_page.dart';
 import 'dart:ui' as ui;
@@ -13,10 +15,24 @@ class MyPainter extends CustomPainter{
     r = Rect.fromLTWH(125.0, 125.0, 175.0, 175.0);
     path.addOval(r);
 
+    canvas.save();
+
     Paint p = Paint();
-    p.color = Color.fromARGB(255, 102, 205, 170);
+    p.color = Color.fromARGB(150, 240, 128, 128);
     p.style = PaintingStyle.fill;
     canvas.drawPath(path, p);
+
+    canvas.translate(0.0, 100.0);
+    p.color = Color.fromARGB(150, 255, 215, 0);
+    canvas.drawPath(path, p);
+
+    p.color = Color.fromARGB(150, 0, 191, 255);
+    canvas.rotate(-0.5 * pi);
+    canvas.translate(-400.0, 50.0);
+    canvas.scale(1 * 1.5);
+    canvas.drawPath(path, p);
+    canvas.restore();
+
   }
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
