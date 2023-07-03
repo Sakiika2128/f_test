e8ac14f18cd6e9553f139007aa21506b122c4c98
Sakiika2128 <sakiikat7064@bside-label.com>
2023-07-03 14:00:08 +0900

    サイズ調節した画像を表示できた

--- lib/my_painter.dart
+++ lib/my_painter.dart
@@ -9,10 +9,12 @@ class MyPainter extends CustomPainter{
   @override
   void paint(Canvas canvas, Size size){
     Paint p = Paint();
+    final _img = this._img;
 
-    Offset off = Offset(50.0, 50.0);
     if(_img != null){
-      canvas.drawImage(_img!, off, p);
+      Rect r0 = Rect.fromLTWH(0.0, 0.0, _img.width.toDouble(), _img.height.toDouble());
+      Rect r = Rect.fromLTWH(50.0, 50.0, 300.0, 200.0);
+      canvas.drawImageRect(_img, r0, r, p);
     }
   }
   @override
