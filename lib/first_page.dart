55c1307026640c20fb8ba00d09c13cd0aa6e0481
Sakiika2128 <sakiikat7064@bside-label.com>
2023-07-14 19:00:29 +0900

    save

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -5,54 +5,55 @@ import 'package:f_test/my_painter.dart';
 import 'dart:typed_data';
 import 'package:flutter/services.dart';
 import 'dart:ui' as ui;
+import 'dart:math';
 
 class MyHomePage extends StatefulWidget{
   const MyHomePage({Key? key}) :super(key: key);
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
-  static ValueNotifier<int> _value = ValueNotifier<int>(0);
+class _MyHomePageState extends State<MyHomePage>
+  with SingleTickerProviderStateMixin{
+    late Animation<double> animation;
+    late AnimationController controller;
 
-  @override
-  void initState(){
-    super.initState();
-  }
+    @override
+    void initState(){
+      super.initState();
+      controller = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 5),
+      );
+      animation = Tween<double>(begin: 0, end: pi * 2)
+        .animate(controller)
+        ..addListener(() {
+          setState(() {
+          });
+        });
+        controller.repeat(reverse: true);
+    }
 
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
-      appBar: AppBar(
-        title: Text(
-          'Graphic demo',
-          style: TextStyle(fontSize: 32),
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Graphic Demo', style: TextStyle(fontSize: 32)),
         ),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(10),
-            ),
-            Container(
-              width: 300,
-              height: 300,
-              child: CustomPaint(
-                painter: MyPainter(_value),
-                child: Center(),
+        body: Center(
+          child: Column(
+            children: [
+              Padding( padding: EdgeInsets.all(10)),
+              Container(
+                width: 300,
+                height: 300,
+                child: CustomPaint(
+                  painter: MyPainter(animation.value),
+                  child: Center(),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-            ),
-            ElevatedButton(
-              child: Text('CLICK!', style: TextStyle(fontSize: 30)),
-              onPressed: () => _value.value++,
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
-  }
+      );
+    }
 }
--- lib/my_painter.dart
+++ lib/my_painter.dart
@@ -5,33 +5,31 @@ import 'package:f_test/first_page.dart';
 import 'dart:ui' as ui;
 
 class MyPainter extends CustomPainter{
-  final ValueNotifier<int> _value;
-
-  MyPainter(this._value);
+  final double value;
+  MyPainter(this.value);
 
   @override
   void paint(Canvas canvas, Size size){
     Paint p = Paint();
-    p.color = Color.fromARGB(140, 127, 255, 212);
+    canvas.save();
+
     p.style = PaintingStyle.fill;
-    
-    Rect r;
-    for(var i = 0; i < _value.value; i++){
-      r = Rect.fromLTWH(
-        10 + i * 20, 
-        10 + i * 20, 
-        100, 
-        100
-      );
-      canvas.drawRect(r, p);
-    }
-    r = Rect.fromLTWH(0, 0, size.width, size.height);
-    p.style = PaintingStyle.stroke;
-    p.color = Color.fromARGB(255, 100, 100, 100);
+    p.color = Color.fromARGB(100, 255, 255, 0);
+    Rect r = Rect.fromLTWH(0, 0, 200, 200);
+    canvas.translate(150, 250);
+    canvas.rotate(value);
+    canvas.translate(-100, -100);
     canvas.drawRect(r, p);
-    if(_value.value > 10){
-      _value.value = 0;
-    }
+
+    canvas.restore();
+    p.style = PaintingStyle.stroke;
+    p.strokeWidth = 25;
+    p.color = Color.fromARGB(150, 64, 224, 208);
+    r = Rect.fromLTWH(0, 0, 200, 200);
+    canvas.translate(150, 250);
+    canvas.rotate(value * -1);
+    canvas.translate(-125, -125);
+    canvas.drawRect(r, p);
   }
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
