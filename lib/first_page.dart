dc3b440de0574ae1c1c59669f3522e8d51d3595c
Sakiika2128 <sakiikat7064@bside-label.com>
2023-07-15 14:57:44 +0900

    save

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,12 +1,6 @@
 import 'package:f_test/main.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:f_test/second_page.dart';
-import 'package:f_test/my_painter.dart';
-import 'dart:typed_data';
-import 'package:flutter/services.dart';
-import 'dart:ui' as ui;
-import 'dart:math';
+
 
 class MyHomePage extends StatefulWidget{
   const MyHomePage({Key? key}) :super(key: key);
@@ -14,35 +8,5 @@ class MyHomePage extends StatefulWidget{
 }
 
 class _MyHomePageState extends State<MyHomePage>{
-  bool flg = false;
 
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Animation Demo', style: TextStyle(fontSize: 28)),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Stack(
-          children: [
-            AnimatedContainer(
-              duration: const Duration(seconds: 1),
-              color: flg ? Colors.lightGreen : Colors.pink,
-              width: flg ? 100 : 300,
-              height: flg ? 300 : 100,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            flg = !flg;
-          });
-        },
-        child: const Icon(Icons.priority_high),
-      ),
-    );
-  }
 }
