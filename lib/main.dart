44cf20487bcb68c36f9290b2f995b682377bce10
Sakiika2128 <sakiikat7064@bside-label.com>
2023-07-29 16:58:37 +0900

    google認証用のメソッドを書いた

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,8 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
