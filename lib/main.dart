400e7aaaaba19d93e0a8b10f2262dd85f188cf65
Sakiika2128 <sakiikat7064@bside-label.com>
2023-08-01 14:00:48 +0900

    google_sign_inとfirebase_authを井mポート

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
