aa7744b4e4843685a3e604bbd8bbffeac81e3dc8
Sakiika2128 <sakiikat7064@bside-label.com>
2023-08-01 17:46:02 +0900

    丸をキーボードで動かす(chrome経由のデバック)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -37,20 +39,45 @@ class _MyHomePageState extends State<MyHomePage>{
   }
 }
 
-class SampleGame extends FlameGame{
+class SampleGame extends FlameGame with KeyboardEvents {
   late final paint;
+  late Vector2 _loc;
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
     paint = Paint();
-    paint.style = PaintingStyle.fill;
     paint.color = Colors.blue;
+    _loc = Vector2(100, 100);
   }
-    @override
-    void render(Canvas canvas) {
-      super.render(canvas);
-      final rect = Rect.fromLTWH(100, 100, 100, 100);
-      canvas.drawOval(rect, paint);
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    final rect = Rect.fromLTWH(_loc.x, _loc.y, 100, 100);
+    canvas.drawOval(rect, paint);
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+    RawKeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
+    final _dpos = Vector2(0, 0);
+
+    if(event.character == 'j'){
+      _dpos.x = -10;
     }
+    if(event.character == 'l'){
+      _dpos.x = 10;
+    }
+    if(event.character == 'i'){
+      _dpos.y = -10;
+    }
+    if(event.character == 'k'){
+      _dpos.y = 10;
+    }
+    _loc += _dpos;
+    return KeyEventResult.handled;
+  }
 }
